07a50406d19cf8e918ab48239a0a7421ffc33ed4
MaximeD <maximedelvenne@hotmail.com>
2026-08-13 11:08:33 +0200

    meh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { IgdbSearchPageComponent } from './features/igdb/pages/igdb-search-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'igdb' },
   { path: 'igdb', loadComponent: () => import('./features/igdb/pages/igdb-search-page.component').then(m => m.IgdbSearchPageComponent) },
-  { path: '**', redirectTo: 'igdb' }
+  { path: '**', redirectTo: 'igdb' },
+  {path: 'igdb-ssearch', component: IgdbSearchPageComponent},
+  { path: '', redirectTo: 'igdb-search', pathMatch: 'full' },
 ];
